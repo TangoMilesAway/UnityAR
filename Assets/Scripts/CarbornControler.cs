b179d823917553a781632ffe117e39431de142c1
tianyi_ma <tianyi_ma@foxmail.com>
2019-09-18 20:23:56 +0800

    第一天工程

---
+++ Assets/Scripts/CarbornControler.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarbornControler : MonoBehaviour
+{
+    public GameObject car;
+    public bool isborn;
+    public float cooldown;
+    public float timer;
+    public GameObject bornpoint;
+    public GameObject endpoint;
+    public int carnum;
+    Vector3 length;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = Random.Range(0,2f);
+
+        carnum = 0;
+        isborn = false;
+        length = this.GetComponent<Renderer>().bounds.size;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isborn&&carnum<=3)
+        {
+            GameObject newcar = Instantiate(car);
+            newcar.transform.parent = this.transform;
+            newcar.transform.position = bornpoint.transform.position;
+            newcar.GetComponent<Carmovecontroler>().endpoint = endpoint;
+            isborn = false;
+            carnum++;
+            timer = Random.Range(0,2f);
+        }
+
+        if (!isborn) {
+            timer += Time.deltaTime;
+            if (timer >= cooldown)
+            {
+                isborn = true;
+            }
+        }
+    }
+
+    public void carnumdown() {
+        carnum--;
+    }
+}
---
+++ Assets/Scripts/Roadborncontroler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roadborncontroler : MonoBehaviour
+{
+    public GameObject road;
+    public GameObject startpoint;
+    public int roadnum;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 1; i <= roadnum; i++)
+        {
+            GameObject newroad = Instantiate(road);
+            newroad.transform.position = startpoint.transform.position + new Vector3(road.GetComponent<Renderer>().bounds.size.x,0,0) * (i - 1);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
